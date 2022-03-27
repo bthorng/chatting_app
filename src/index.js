fc341574e55810051ceb3a4ea741c0a21a319987
Brandon Thorng <brandonthorng@gmail.com>
2022-03-26 21:02:00 -0400

    Configured base project

---
+++ src/index.js
@@ -0,0 +1,16 @@
+import { render } from "react-dom";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import App from "./App";
+import Chat from "./routes/chat";
+
+const rootElement = document.getElementById("root");
+render(
+	<BrowserRouter>
+		<Routes>
+			<Route path="/" element={<App />}>
+				<Route path="" element={<Chat />} />
+			</Route>
+		</Routes>
+	</BrowserRouter>,
+	rootElement
+);
